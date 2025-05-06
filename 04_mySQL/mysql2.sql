9e65a8a29d95475ad67c8131b743806e3995055e
AK47 <147317580+Anish-Kanrar@users.noreply.github.com>
2025-05-06 22:41:56 +0530

    Update mysql2.sql

--- 04_mySQL/mysql2.sql
+++ 04_mySQL/mysql2.sql
@@ -21,9 +21,34 @@ VALUES
 (3, 16, "casey", "casey@gmail.com", 523, 155),
 (4, 17, "donald", "donald@gmail.com", 129, 445);
 
-SELECT * FROM user
-WHERE followers <= 200;
+-- SELECT name,age
+--  FROM user
+-- WHERE age < 16;
 
+-- SELECT name, age, followers
+-- FROM user
+-- WHERE age > 16	OR followers > 200;
+
+-- SELECT name, age, followers
+-- FROM user
+-- WHERE age BETWEEN 14 AND 16;
+
+-- SELECT name, age, email
+-- FROM user
+-- WHERE age IN (14, 16);
+
+-- SELECT name, age, email
+-- FROM user
+-- WHERE age NOT IN (14, 16);
+
+-- SELECT name, age, email
+-- FROM user
+-- WHERE age > 14
+-- LIMIT 2;
+
+SELECT name, age, followers
+FROM user
+ORDER BY followers ASC;
 
 CREATE TABLE Posts(
 	id INT PRIMARY KEY,
@@ -33,4 +58,4 @@ CREATE TABLE Posts(
 );
 
 -- SELECT * FROM user;
-SELECT id, age, name FROM user;
+-- SELECT id, age, name FROM user;
